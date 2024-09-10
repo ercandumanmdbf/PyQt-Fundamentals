3884a78dea5e3635acf7d5288c8415704d104352
Ercan Duman <ercanduman11@gmail.com>
2024-09-10 21:58:44 +0300

    create window

---
+++ window.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 10 21:54:10 2024
+
+@author: ercan
+"""
+
+import sys
+from PyQt5 import QtWidgets
+
+def source():
+    object=QtWidgets.QApplication(sys.argv)
+    window=QtWidgets.QWidget()
+    
+    window.setWindowTitle("PyQt5 Arayüz Penceresi") #window title
+    window.setGeometry(250,300,600,300) #(xpos,ypos,width,height)
+    
+    window.show()   
+    sys.exit(object.exec_())
+source()
